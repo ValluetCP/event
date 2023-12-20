da138d622764272c2d1b3241f41c9c94eb43efd7
CYNTHIA <cynthia.petitot@gmail.com>
2023-12-20 14:45:30 +0100

    panier_2.php (suppression des doublons)
    nbr total de place par event
    prix total par event
    prix total du panier

--- views/panier_2.php
+++ views/panier_2.php
@@ -13,16 +13,50 @@ if (isset($_SESSION['id_user'], $_GET['event'], $_POST['place_reserve'])) {
 
 // Récupérer les événements dans le panier
 if (isset($_SESSION['panier']) && !empty($_SESSION['panier'])) {
+    // Utiliser un tableau associatif pour éviter les doublons
+    $panierItems = array();
+    $totalMontant = 0;
+
     foreach ($_SESSION['panier'] as $item) {
         // Récupérer les détails de l'événement en utilisant l'ID de l'événement
         $eventId = $item[1];
         $eventDetails = Event::findEventById($eventId);
 
-        // Afficher le titre de l'événement
-        echo "Titre de l'événement : " . $eventDetails['titre'] . "<br>"."Catégorie : " . $eventDetails['categorie_name']."<br>"."Prix : " . $eventDetails['prix']. "<br><br>";
+        // Récupérer le nombre de places réservées pour cet événement
+        $placesReservees = $item[2];
+
+        // Vérifier si l'événement est déjà dans le panier
+        if (array_key_exists($eventId, $panierItems)) {
+            // L'événement est déjà dans le panier, mettre à jour la quantité et le prix
+            $panierItems[$eventId]['quantity'] += $placesReservees;
+            $panierItems[$eventId]['price'] += $eventDetails['prix'] * $placesReservees;
+        } else {
+            // L'événement n'est pas encore dans le panier, l'ajouter
+            $panierItems[$eventId] = array(
+                'eventDetails' => $eventDetails,
+                'quantity' => $placesReservees,
+                'price' => $eventDetails['prix'] * $placesReservees
+            );
+        }
+
+        // Ajouter le prix total de l'événement au total général
+        $totalMontant += $panierItems[$eventId]['price'];
+    }
+
+    // Afficher les éléments du panier
+    foreach ($panierItems as $itemId => $itemDetails) {
+        echo "Titre de l'événement : " . $itemDetails['eventDetails']['titre'] . "<br>";
+        echo "Catégorie : " . $itemDetails['eventDetails']['categorie_name'] . "<br>";
+        echo "Prix unitaire : " . $itemDetails['eventDetails']['prix'] . "<br>";
+        echo "Quantité : " . $itemDetails['quantity'] . "<br>";
+        echo "Prix total : " . $itemDetails['price'] . "<br><br>";
         // Vous pouvez afficher d'autres détails de l'événement si nécessaire
     }
+
+    // Afficher le montant total en bas de la page
+    echo "Montant total : " . $totalMontant . "<br>";
 }
 
 // ... Le reste de votre code panier.php ...
 ?>
+</a>
